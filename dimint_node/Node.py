f2601cf1a6d8caf31e4d7448d2714cc9e0fb2314
jsryu21 <jsryu21@snu.ac.kr>
2014-12-05 21:30:09 +0900

    Node encoding and etc...

--- dimint_node/Node.py
+++ dimint_node/Node.py
@@ -2,23 +2,25 @@ import threading, zmq, json
 
 class Node(threading.Thread):
     def __init__(self, host, port, pull_port):
+        threading.Thread.__init__(self)
         self.host = host
         self.port = port
         self.address = 'tcp://{0}:{1}'.format(self.host, self.port)
         context = zmq.Context()
         self.pull_socket = context.socket(zmq.PULL)
-        self.pull_socket.bind('tcp://{0}:{1}'.format(self.host, self.pull_port))
+        self.pull_socket.bind('tcp://{0}:{1}'.format(self.host, pull_port))
         self.socket = context.socket(zmq.DEALER)
         self.socket.connect(self.address)
         self.storage = {}
 
     def run(self):
         while True:
-            message = self.socket.recv()
+            ident, message = self.pull_socket.recv_multipart()
+            print(message)
             result = self.__process(message)
             response = json.dumps(result).encode('utf-8')
             print (response)
-            self.socket.send_multipart(response)
+            self.socket.send_multipart([ident, response])
 
     def __process(self, message):
         print('Request {0}'.format(message))
@@ -26,7 +28,6 @@ class Node(threading.Thread):
             request = json.loads(message.decode('utf-8'))
             cmd = request['cmd']
             key = request['key']
-            response = {}
             if cmd == 'get':
                 value = self.storage[key]
             elif cmd == 'set':
@@ -34,5 +35,6 @@ class Node(threading.Thread):
                 self.storage[key] = value
         except:
             value = None
+        response = {}
         response['value'] = value
         return response
--- dimint_node/test_node.py
+++ dimint_node/test_node.py
@@ -1,4 +1,4 @@
 from Node import Node
 
-node = Node('127.0.0.1', 5557)
-node.run()
+node = Node('127.0.0.1', 5557, 15556)
+node.start()
