c3a254826eff68ae640436b11e2d12b9675a9f33
Kim, Jae Chan <kjc0210@gmail.com>
2014-12-12 14:20:43 +0900

    send cmd to overlord when connect, and handle response

--- dimint_node/Node.py
+++ dimint_node/Node.py
@@ -1,10 +1,17 @@
-import threading, zmq, json
+import json
+import socket
+import threading
+
+from kazoo.client import KazooClient
+import zmq
+
 
 class Node(threading.Thread):
     def __init__(self, host, port, pull_port):
         threading.Thread.__init__(self)
         self.host = host
         self.port = port
+        self.pull_port = pull_port
         self.address = 'tcp://{0}:{1}'.format(self.host, self.port)
         context = zmq.Context()
         self.pull_socket = context.socket(zmq.PULL)
@@ -12,6 +19,7 @@ class Node(threading.Thread):
         self.socket = context.socket(zmq.DEALER)
         self.socket.connect(self.address)
         self.storage = {}
+        self.__connect()
 
     def run(self):
         while True:
@@ -38,3 +46,32 @@ class Node(threading.Thread):
         response = {}
         response['value'] = value
         return response
+
+    def __connect(self):
+        connect_request = {'cmd': 'connect',
+                           'ip': self.get_ip(),
+                           'cmd_receive_port': self.pull_port,
+                           'transfer_port': 5575,  # temp
+                           'master_receive_port': 5572,  # temp
+                           }
+        self.socket.send_multipart([b'', json.dumps(connect_request).encode('utf-8')])
+        ident, recv_data = self.socket.recv_multipart()
+        print('Response:{0}'.format(recv_data))
+        recv_data = json.loads(recv_data.decode('utf-8'))
+        self.node_id = recv_data.get('node_id')
+        self.role = recv_data.get('role')
+        self.zookeeper_hosts = recv_data.get('zookeeper_hosts')
+        self.register_to_zookeeper()
+
+    def get_ip(self):
+        return [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close())
+                for s in [socket.socket(socket.AF_INET,
+                                        socket.SOCK_DGRAM)]][0][1]
+
+    def register_to_zookeeper(self):
+        self.zk = KazooClient(self.zookeeper_hosts)
+        self.zk.start()
+
+        self.zk.ensure_path('/dimint/node/list')
+        self.zk.create('/dimint/node/list/{0}'.format(self.node_id),
+                       ephemeral=True)
