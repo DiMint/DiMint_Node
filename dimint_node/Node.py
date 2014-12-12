66a3dd5eace418b3996eb7f0f061f71f1d7399a6
Kim, Jae Chan <kjc0210@gmail.com>
2014-12-12 19:32:03 +0900

    handle about role when connected to overlord

--- dimint_node/Node.py
+++ dimint_node/Node.py
@@ -7,28 +7,42 @@ import zmq
 
 
 class Node(threading.Thread):
-    def __init__(self, host, port, pull_port):
+    def __init__(self, host, port, pull_port, push_to_slave_port):
         threading.Thread.__init__(self)
         self.host = host
         self.port = port
         self.pull_port = pull_port
+        self.push_to_slave_port = push_to_slave_port
         self.address = 'tcp://{0}:{1}'.format(self.host, self.port)
-        context = zmq.Context()
-        self.pull_socket = context.socket(zmq.PULL)
+        self.context = zmq.Context()
+        self.pull_socket = self.context.socket(zmq.PULL)
         self.pull_socket.bind('tcp://{0}:{1}'.format(self.host, pull_port))
-        self.socket = context.socket(zmq.DEALER)
+        self.socket = self.context.socket(zmq.DEALER)
         self.socket.connect(self.address)
         self.storage = {}
+        self.push_to_slave_socket = self.context.socket(zmq.PUSH)
+        self.push_to_slave_socket.bind('tcp://*:{0}'.format(self.push_to_slave_port))
+        self.pull_from_master_socket = None
         self.__connect()
 
     def run(self):
+        poll = zmq.Poller()
+        poll.register(self.pull_socket, zmq.POLLIN)
+        if self.pull_from_master_socket is not None:
+            poll.register(self.pull_from_master_socket, zmq.POLLIN)
+
         while True:
-            ident, message = self.pull_socket.recv_multipart()
-            print(message)
-            result = self.__process(message)
-            response = json.dumps(result).encode('utf-8')
-            print (response)
-            self.socket.send_multipart([ident, response])
+            sockets = dict(poll.poll())
+            if self.pull_socket in sockets:
+                ident, message = self.pull_socket.recv_multipart()
+                print(message)
+                result = self.__process(message)
+                response = json.dumps(result).encode('utf-8')
+                print (response)
+                self.socket.send_multipart([ident, response])
+            else:
+                # TODO: handle message from master node
+                pass
 
     def __process(self, message):
         print('Request {0}'.format(message))
@@ -52,7 +66,7 @@ class Node(threading.Thread):
                            'ip': self.get_ip(),
                            'cmd_receive_port': self.pull_port,
                            'transfer_port': 5575,  # temp
-                           'master_receive_port': 5572,  # temp
+                           'push_to_slave_port': self.push_to_slave_port,
                            }
         self.socket.send_multipart([b'', json.dumps(connect_request).encode('utf-8')])
         ident, recv_data = self.socket.recv_multipart()
@@ -61,7 +75,13 @@ class Node(threading.Thread):
         self.node_id = recv_data.get('node_id')
         self.role = recv_data.get('role')
         self.zookeeper_hosts = recv_data.get('zookeeper_hosts')
+
         self.register_to_zookeeper()
+
+        if self.role == 'slave':
+            self.pull_from_master_socket = self.context.socket(zmq.PULL)
+            self.pull_from_master_socket.connect(recv_data.get('master_addr'))
+        self.socket.close()
 
     def get_ip(self):
         return [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close())
--- dimint_node/test_node.py
+++ dimint_node/test_node.py
@@ -1,4 +1,7 @@
 from Node import Node
 
-node = Node('127.0.0.1', 5557, 15556)
+node = Node('127.0.0.1', 5557, 15556, 5558)
 node.start()
+
+node2 = Node('127.0.0.1', 5557, 15557, 5561)
+node2.start()
